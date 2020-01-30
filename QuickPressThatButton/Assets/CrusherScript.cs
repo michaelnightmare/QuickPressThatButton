89bd346cb035e496049a3c65fe5697d5bf532a61
michaelnightmare <michaelsmith0802@gmail.com>
2020-01-29 23:09:30 -0800

    InitialPush

---
+++ QuickPressThatButton/Assets/CrusherScript.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrusherScript : MonoBehaviour {
+
+    public GameObject Crusher;
+    public GameObject ButtonPressed;
+
+    float t;
+    float pauseTime;
+    Vector2 startPosition;
+    public GameObject target;
+    public float timeToReachTarget;
+
+    // Use this for initialization
+    void Start ()
+    {
+        startPosition = Crusher.transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+       
+
+        t += Time.deltaTime / timeToReachTarget;
+        transform.position = Vector2.Lerp(startPosition, target.transform.position, t);
+    }
+
+    
+}
