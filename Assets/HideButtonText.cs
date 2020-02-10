bdbaea818a60cacee10232114e7333b43623b921
michaelnightmare <michaelsmith0802@gmail.com>
2020-02-10 01:32:03 -0800

    Functioning

---
+++ Assets/HideButtonText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideButtonText : MonoBehaviour
+{
+    public GameObject buttonText;
+
+    void OnEnable()
+    {
+        StartCoroutine(RemoveAfterSeconds(.5f, gameObject));
+    }
+
+    IEnumerator RemoveAfterSeconds(float seconds, GameObject obj)
+    {
+        yield return new WaitForSeconds(.5f);
+        obj.SetActive(false);
+    }
+
+
+}
